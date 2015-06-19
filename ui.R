e8b40fd0116755ce01a7455c40011c7e36fb4568
Kevin Beard <kevinbeard67@gmail.com>
2015-06-18 23:05:44 -0400

    Update 2
    changed some graphics and edited app

--- ui.R
+++ ui.R
@@ -3,11 +3,11 @@ shinyUI(pageWithSidebar(
   headerPanel("Dog Age Calculator"),
  
   sidebarPanel(
-    p(strong('Using the sliders, enter the age of your dog.')),
+    p(strong('Using the sliders, enter the calendar age of your dog.')),
     br(),
     
     sliderInput('yr' , 'Number of years?', value = 5, min = 0, max = 20, step = 1 ),
-    sliderInput('mo', '...and number of months?', value = 0, min = 0, max = 11, step = 1 ),
+    sliderInput('mo', 'Number of months?', value = 0, min = 0, max = 11, step = 1 ),
     submitButton('Submit'),
  
  
@@ -17,11 +17,12 @@ shinyUI(pageWithSidebar(
  
   mainPanel(
  
-    h1("So, just how old is my dog in ", em( "Human Years"), "?"),
+    h1("So, just how ", strong("old") , " is my dog?"),
     
     p("Dogs mature faster than humans, reaching the equivalent
-    of twenty-one years in only about two years of age, but then aging slows to an average of four 
-    human years every year after."), 
+    of twenty-one human years, in only about two calendar years of age, but then aging slows to an average of four 
+    human years every year after. The age of a dog, if they were human, are often referred to 
+    as ",  em( "Dog Years"), "."), 
 
     br(),
 
@@ -29,7 +30,7 @@ shinyUI(pageWithSidebar(
       probably say",em(" Multiply by seven. "), "However, this method is inaccurate, and 
       more so the older a dog gets. For a twelve year-old dog, the result could be 
       off by over twenty years. Multiplying by seven only comes close while a dog 
-      is about four to five years old. "),
+      is about seven years old. "),
     
     br(),
     
@@ -41,11 +42,10 @@ shinyUI(pageWithSidebar(
     br(),
     br(),
 
-    h3('Your age of your dog ...'),
-    h4('in Human years is about: '),
+    h3('Your age of your dog in human or calendar years is about: '),
     verbatimTextOutput("hyr"),
     
-    h4('While in Dog years is about: '),
+    h3('While their age in Dog Years is about: '),
     verbatimTextOutput("dyr")
   )
 ))
